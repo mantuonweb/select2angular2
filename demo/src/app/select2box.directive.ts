e966880d75e2dec90f34ad1779ff7afa43c850cf
SYNAPSE\mantu.nigam <mantu.nigam@globallogic.com>
2018-03-07 10:22:51 +0530

    some minor changes

--- demo/src/app/select2box.directive.ts
+++ demo/src/app/select2box.directive.ts
@@ -1,4 +1,4 @@
-import { Directive,ElementRef,Input,Output, EventEmitter } from '@angular/core';
+import { Directive, ElementRef, Input, Output, EventEmitter } from '@angular/core';
 import { NgModel } from '@angular/forms';
 declare var $;
 @Directive({
@@ -6,28 +6,26 @@ declare var $;
   providers: [NgModel]
 })
 export class Select2boxDirective {
-  @Input() datalist:any;
-  @Output() onchangedata=new EventEmitter<any>();
-  $select:any;
+  @Input() datalist: any;
+  @Output() onchangedata = new EventEmitter<any>();
+  $select: any;
   constructor(public _elementRef: ElementRef, private ngModel: NgModel) {
   }
   ngAfterViewInit() {
-    this.$select=$(this._elementRef.nativeElement);
+    this.$select = $(this._elementRef.nativeElement);
     this.$select.select2({
       'data': this.datalist,
       'theme': "bootstrap"
-    })
-    this.$select.on('select2:select', e=> {
+    });
+    this.$select.on('select2:select', e => {
       var data = e.params.data;
-      var eledata={id:"",text:""};
-      eledata.id=data.id;
-      eledata.text=data.text;
+      var eledata = { id: "", text: "" };
+      eledata.id = data.id;
+      eledata.text = data.text;
       this.ngModel.update.emit(eledata);
     });
-    this.ngModel.valueChanges.subscribe(value=> {
+    this.ngModel.valueChanges.subscribe(value => {
       this.$select.val(value.id).trigger('change');
-      //$("#select2-myInput-container").text(value.text);
-      //console.log($(this.$select.closest(".select2")));
       $($(this.$select.siblings(".select2")).find("#select2-myInput-container")).text(value.text);
     });
   }
